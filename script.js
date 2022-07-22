b4cc0b29f9c606171d79d86a67358b06dcbd2140
renyellen <reny1934@gmail.com>
2022-07-22 09:57:12 -0300

    desafio ingles

---
+++ script.js
@@ -0,0 +1,24 @@
+let comedy={
+    title:"Legalmente loira",
+    director:"Robert",
+    year:2001,
+}
+
+let fantasia={
+    title:"Narnia",
+    director:"Maycon",
+    year:2005,
+}
+
+let acao={
+    title:"Creed",
+    director:"Silvester Stallone",
+    year:2015,
+}
+
+let filmes=false;
+if(filmes===false){
+    console.log("Meu filme favorito é Titanic")
+}else{
+    console.log("Não vale nem 5 estrelas")
+}
